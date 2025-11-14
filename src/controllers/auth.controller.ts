a44a803477c593647f7de3e708a0b3f0fcddb648
shasbinas <shasbinas1@gmail.com>
2025-11-14 23:09:34 +0530

    wip:auth routes

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,29 @@
 import type { Request, Response } from 'express';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { authService } from '../services/auth.service.js';
+import { ApiError } from '../utils/ApiError.js';
 
 export const registerUser = asyncHandler(async (req: Request, res: Response) => {
   const user = await authService.registerUser(req.body);
   res.status(201).json(user);
+});
+
+export const loginUser = asyncHandler(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const { user, token } = await authService.loginUser(email, password);
+  res.status(200).json({
+    _id: user._id,
+    username: user.username,
+    email: user.email,
+    token,
+  });
+});
+
+export const logoutUser = asyncHandler(async (req: Request, res: Response) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) throw ApiError.badRequest('Token required');
+
+  await authService.logoutUser(token);
+  res.status(200).json({ message: 'Logged out successfully' });
 });
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/auth.controller.js';
+import { loginUser, logoutUser, registerUser } from '../controllers/auth.controller.js';
 
 const authRoutes = Router({ mergeParams: true });
 
 // POST /api/auth/register
 authRoutes.post('/register', registerUser);
+authRoutes.post('/login', loginUser);
+authRoutes.post('/logout', logoutUser);
 
 export default authRoutes;

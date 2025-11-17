a737fd67027d48e02fbaa9c4520aaaacbcb07b0b
shasbinas <shasbinas1@gmail.com>
2025-11-17 15:56:43 +0530

    wip:jest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  clearMocks: true,
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
-  moduleFileExtensions: ["ts", "js"],
-  testMatch: ["**/tests/**/*.test.ts"],
-};
